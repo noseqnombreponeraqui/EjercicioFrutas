e042d0732d2e1d875f2b5377ef8676afaabb8f66
noseqnombreponeraqui <40415148+noseqnombreponeraqui@users.noreply.github.com>
2020-06-01 21:21:59 -0400

    [MS]Avance Frutas

---
+++ Frutas/src/Frutas/MainFrutaRicaFruta.java
@@ -0,0 +1,36 @@
+package Frutas;
+
+import java.util.Scanner;
+
+public class MainFrutaRicaFruta {
+
+	public MainFrutaRicaFruta() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//instancia de la clase
+		Fruta Fruta = new Fruta();
+		
+		Manzana manzana1 = new Manzana();
+		Manzana manzana2 = new Manzana("Roja","Dulce","Grande","Fruto Maduro");
+		Manzana manzana3 = new Manzana("Manzana Royal");
+		
+		Uva uva1 = new Uva();
+		Uva uva2 = new Uva("Verde","Dulce","Grande","Fruto Maduro");
+		Uva uva3 = new Uva("Uva Blanca","Chillan");
+		
+		Fruta Fruta1 = new Manzana();
+		Fruta Fruta2 = new Uva();
+		
+		System.out.println("ingrese color de la fruta");
+		String fcolor = sc.next();
+		Fruta1.setcolor(fcolor);
+		
+		System.out.println(Fruta1.getcolor());
+		
+	}
+
+}
